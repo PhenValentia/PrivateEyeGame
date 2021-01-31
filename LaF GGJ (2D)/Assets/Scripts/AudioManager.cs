af2f054ef6b3e46f4179a2518d89a5d74fb83c5c
PhenValentia <salternicholaS@yahoo.com>
2021-01-31 20:53:04 +0000

    main

---
+++ LaF GGJ (2D)/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour // Writen by Phen Valentia (Nicholas Salter) and Trang
+{
+    // Audio files
+    public AudioSource NRoomBackgroundMusic;
+    public AudioSource ERoomBackgroundMusic;
+
+    // Audio
+    public float currentAudioMax = 0.1f;
+
+    void Start()
+    {
+        // Find AudioSource Attached to GameManager
+        AudioSource[] AMs = GetComponents<AudioSource>();
+        NRoomBackgroundMusic = AMs[0];
+        ERoomBackgroundMusic = AMs[1];
+        currentAudioMax = NRoomBackgroundMusic.volume;
+        NRoomBackgroundMusic.Play();
+        ERoomBackgroundMusic.Play();
+        NRoomBackgroundMusic.volume = currentAudioMax;
+        ERoomBackgroundMusic.volume = 0f;
+        NRoomBackgroundMusic.loop = true;
+        ERoomBackgroundMusic.loop = true;
+    }
+
+    // Play n-room background music
+    public void playNRoomBackgroundMusic()
+    {
+        // Stop Previous Music from Playing
+        NRoomBackgroundMusic.Stop();
+        ERoomBackgroundMusic.Stop();
+        // Load in New Music
+        NRoomBackgroundMusic.clip = Resources.Load<AudioClip>("AudioSources/MainRoomN");
+        // Start New Music
+        NRoomBackgroundMusic.Play();
+    }
+
+    // Play e-room background music
+    public void playERoomBackgroundMusic()
+    {
+        // Stop Previous Music from Playing
+        NRoomBackgroundMusic.Stop();
+        ERoomBackgroundMusic.Stop();
+        // Load in New Music
+        ERoomBackgroundMusic.clip = Resources.Load<AudioClip>("AudioSources/MainRoomE");
+        // Start New Music
+        ERoomBackgroundMusic.Play();
+    }
+
+    public void playEvil()
+    {
+        // Stop Previous Music from Playing
+        NRoomBackgroundMusic.Stop();
+        ERoomBackgroundMusic.Stop();
+        // Load in New Music
+        ERoomBackgroundMusic.clip = Resources.Load<AudioClip>("AudioSources/End");
+        // Start New Music
+        ERoomBackgroundMusic.Play();
+    }
+
+    public void playEnd()
+    {
+        // Stop Previous Music from Playing
+        NRoomBackgroundMusic.Stop();
+        ERoomBackgroundMusic.Stop();
+        NRoomBackgroundMusic.loop = false;
+        // Load in New Music
+        NRoomBackgroundMusic.clip = Resources.Load<AudioClip>("AudioSources/Stinger");
+        ERoomBackgroundMusic.clip = Resources.Load<AudioClip>("AudioSources/MenuMusic");
+        // Start New Music
+        NRoomBackgroundMusic.Play();
+        ERoomBackgroundMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ LaF GGJ (2D)/Assets/Scripts/Intro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour // Entirely Writen by Phen Valentia (Nicholas Salter)
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel(2);
+    }
+
+}
