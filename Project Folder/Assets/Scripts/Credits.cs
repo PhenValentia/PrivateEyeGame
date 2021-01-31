cae4827fd53b2e33887bbb581a0c1691a643cf4c
PhenValentia <salternicholaS@yahoo.com>
2021-01-31 17:44:58 +0000

    Renames

---
+++ Project Folder/Assets/Scripts/Credits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour // Entirely Writen by Phen Valentia (Nicholas Salter)
+{
+
+    public void quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
